7ffabb450f2a58d8b65fdd309366361435aee038
user1 <user1@example.com>
2019-11-17 16:07:57 +0000

    Delete remote directory before deletion

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,17 @@
 #ftpPassword="password"
 #ftpHost=ftp.example.com
 #ftpPath=/home/BanglaDateJS
+ncftpput -u $ftpUser -p $ftpPassword $ftpHost
+cd $ftpPath
+
+cd coverage
+cd lcov-report
+rm *.*
+cd ..
+rmdir lcov-report
+rm *.*
+cd ..
+rmdir coverage 
+quit
 
 ncftpput -R -v -u $ftpUser -p $ftpPassword $ftpHost $ftpPath "coverage"
